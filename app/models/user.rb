612770484e05252401290a992a826230b713494f
Sunjay Armstead <connect@sunjayarmstead.local>
2023-09-25 15:51:22 -0400

    Add user ROLES and update roles select in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  ROLES = [
+    'guest',
+    'admin',
+    'dino_tamer'
+  ]
+
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
-  validates :role, presence: true
+  validates :role, presence: true, inclusion: { in: ROLES, allow_blank: true }
 end
